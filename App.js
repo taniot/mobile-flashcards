f5d895133019154b3d5f13255e8db319d9416398
taniot <gaetano.frascolla@gmail.com>
2020-07-19 22:16:25 +0200

    chore: add redux

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { View, Dimensions } from "react-native";
 import { NavigationContainer } from '@react-navigation/native'
 import NavigationMenu from './navigation/NavigationMenu'
 import MyStatusBar from './navigation/StatusBar'
@@ -6,9 +7,16 @@ import { main } from './utils/colors'
 
 export default function App() {
   return (
+    <View
+          style={{
+            width: Dimensions.get("window").width,
+            height: Dimensions.get("window").height,
+          }}
+        >
     <NavigationContainer>
       <MyStatusBar backgroundColor={main} barStyle='light-content' />
       <NavigationMenu />
     </NavigationContainer>
+    </View>
   )
 }
---
+++ utils/_DATA.js
@@ -0,0 +1,47 @@
+import { AsyncStorage } from 'react-native'
+export const APP_STORAGE_KEY = 'MobileFlashCards:appkey';
+
+//database
+
+export const data = {
+    JavaScript: {
+        title: 'Countries',
+        questions: [
+            {
+                question: 'What is the diameter of Earth?',
+                answer: '8,000 miles'
+            },
+            {
+                question: 'What is the capital city of Spain?',
+                answer: 'Madrid'
+            },
+            {
+                question: 'Which country is Prague in?',
+                answer: 'Czech Republic'
+            }
+        ]
+    },
+    React: {
+        title: 'Sports',
+        questions: [
+            {
+                question: 'Which sport does Constantino Rocca play?',
+                answer: 'Golf'
+            },
+            {
+                question: 'Which chess piece can only move diagonally?',
+                answer: 'A bishop'
+            }
+        ]
+    }
+}
+
+
+function initialize() {
+    AsyncStorage.setItem(APP_STORAGE_KEY, JSON.stringify(data))
+    return data
+}
+
+export function formatDeckResults(results) {
+    return results === null ? initialize() : JSON.parse(results)
+}
